93946dc9ccb15123b98800add9f0aedad02e3a73
Thomas Sayen <69324626+Chi-Iroh@users.noreply.github.com>
2023-07-21 11:53:24 +0200

    [ADD] : Minimal window

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,25 @@
+#include <SFML/Graphics.hpp>
+
 int main() {
+    const sf::VideoMode windowSize{ 500, 500 };
+    sf::RenderWindow window{
+        windowSize,
+        "Koi-Koi",
+        sf::Style::Default,
+    };
+
+    while (window.isOpen()) {
+        sf::Event event{};
+
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+
     return 0;
 }
